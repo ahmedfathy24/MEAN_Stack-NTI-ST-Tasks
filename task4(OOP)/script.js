78ada55fa2cbb3fce30d393a7217367c880302f0
Ahmed Fathy <ahmed417999@feng.bu.edu.eg>
2025-07-02 13:00:27 +0300

    Add task4 files

---
+++ task4(OOP)/script.js
@@ -0,0 +1,7 @@
+import { Rectangle, Square, Circle } from './shapes.js';
+const rect = new Rectangle(5, 10);
+const square = new Square(6);
+const circle = new Circle(4);
+console.log(rect);
+console.log(square.toString());
+console.log(circle.toString());
---
+++ task4(OOP)/shapes.js
@@ -0,0 +1,58 @@
+class Shape {
+    constructor(name) {
+        if (this.constructor === Shape) {
+            throw new Error("Abstract class");
+        }
+        this.name = name;
+    }
+    getArea() {
+    return 0;
+    }
+    getPerimeter() {
+    return 0;
+    }
+    toString() {
+    return `Area of ${this.name}: ${this.getArea()}\nPerimeter of ${this.name}: ${this.getPerimeter()}`;
+    }
+}
+class Square extends Shape {
+    constructor(w, name = "Square") {
+        super(name);
+        this.side = w;
+    }
+    getArea() {
+    return this.side * this.side;
+    }
+    getPerimeter() {
+    return 4 * this.side;
+    }
+}
+class Rectangle extends Square {
+    constructor(w ,h) {
+        super(w, "Rectangle");
+        this.width = w;
+        this.height = h;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+    getPerimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+class Circle extends Shape {
+    constructor(r) {
+    super("Circle");
+    this.radius = r;
+    }
+    getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+    getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+    toString() {
+        return `Area of ${this.name}: ${this.getArea()}\nPerimeter of ${this.name}: ${this.getPerimeter()}\nRadius: ${this.radius}`;
+    }
+}
+export { Shape, Square, Rectangle, Circle };
